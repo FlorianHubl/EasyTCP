dbbddabd8a9691051667151f8d2a916fdb06c224
flohubl <flohubl@smail.hs-bremerhaven.de>
2023-10-05 11:46:35 +0200

    changed test

--- Tests/EasyTCPTests/EasyTCPTests.swift
+++ Tests/EasyTCPTests/EasyTCPTests.swift
@@ -4,9 +4,9 @@ import XCTest
 final class EasyTCPTests: XCTestCase {
     @available(iOS 13.0, *)
     func testExample() async throws {
-        let tcp = EasyTCP(hostName: "electrum.blockstream.info", port: 50002, using: .tls)
+        let tcp = EasyTCP(hostName: "tcpbin.com", port: 4242)
         tcp.start()
-        let a = try await tcp.send("{\"jsonrpc\": \"2.0\", \"method\": \"server.version\", \"params\": [\"\", \"1.4\"], \"id\": 1}")
+        let a = try await tcp.send("Hello")
         let b = String(data: a, encoding: .utf8)!
         print(b)
     }
